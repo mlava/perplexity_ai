b10e5faaea4ed30789e27cc9ab366fc21d07594c
Mark Lavercombe <mlava@users.noreply.github.com>
2024-12-13 09:15:38 +1100

    Update extension.js

--- extension.js
+++ extension.js
@@ -10,14 +10,20 @@ const config = {
         {
             id: "pplx-model",
             name: "Prefered Model",
-            description: "API Key from https://www.perplexity.ai/settings/api",
-            action: { type: "select", items: ["Today Only", "2", "3", "4", "5", "6", "7", "8"] },
+            description: "Preferred language model",
+            action: { type: "select", items: ["Preferred Model", "llama-3.1-sonar-small-128k-online", "llama-3.1-sonar-large-128k-online", "llama-3.1-sonar-huge-128k-online"] },
         },
         {
             id: "pplx-sources",
             name: "Include Sources",
             description: "Import sources for each answer",
             action: { type: "switch" },
+        },
+        {
+            id: "pplx-instructions",
+            name: "Default instructions",
+            description: "Default system prompt / instructions",
+            action: { type: "input", placeholder: "" },
         },
     ]
 };
@@ -39,22 +45,29 @@ export default {
                         await window.roamAlphaAPI.updateBlock({
                             block: { uid: childUid, string: blocks[0].text.toString(), }
                         });
-                        const sourceUid = window.roamAlphaAPI.util.generateUID();
-                        window.roamAlphaAPI.createBlock({ "location": { "parent-uid": uid, "order": 1 }, "block": { "string": "Sources:", "uid": sourceUid, "children-view-type": "numbered" } });
-
-                        blocks = blocks[0].children;
-                        blocks.forEach((node, order) => createBlock({
-                            sourceUid,
-                            order,
-                            node
-                        }));
+                        if (blocks[0].hasOwnProperty("children")) {
+                            const sourceUid = window.roamAlphaAPI.util.generateUID();
+                            window.roamAlphaAPI.createBlock({ "location": { "parent-uid": uid, "order": 1 }, "block": { "string": "Sources:", "uid": sourceUid, "children-view-type": "numbered" } });
+    
+                            blocks = blocks[0].children;
+                            blocks.forEach((node, order) => createBlock({
+                                sourceUid,
+                                order,
+                                node
+                            }));
+                            const followUpUid = window.roamAlphaAPI.util.generateUID();
+                            window.roamAlphaAPI.createBlock({ "location": { "parent-uid": uid, "order": 2 }, "block": { "string": "Follow-Up:", "uid": followUpUid, } });
+                            const followUpUid1 = window.roamAlphaAPI.util.generateUID();
+                            window.roamAlphaAPI.createBlock({ "location": { "parent-uid": followUpUid, "order": 0 }, "block": { "string": "", "uid": followUpUid1, } });
+                         }
                     })
                 }
             }
         });
 
         async function fetchPplx(uid, childUid) {
-            var key;
+            var key, model, instructions;
+            var showSources = true;
             breakme: {
                 if (!extensionAPI.settings.get("pplx-apiKey")) {
                     key = "API";
@@ -62,34 +75,71 @@ export default {
                     break breakme;
                 } else {
                     window.roamAlphaAPI.createBlock({ "location": { "parent-uid": uid, "order": 0 }, "block": { "string": "Loading...", "uid": childUid } });
-
                     var apiKey = extensionAPI.settings.get("pplx-apiKey");
-                    var pplx_url = "https://api.perplexity.ai/chat/completions";
+                    if (extensionAPI.settings.get("pplx-model") != "Preferred Model") {
+                        model = extensionAPI.settings.get("pplx-model");
+                    } else {
+                        model = "llama-3.1-sonar-small-128k-online";
+                    }
+                    if (extensionAPI.settings.get("pplx-sources") == false) {
+                        showSources = false;
+                    }
+                    if (extensionAPI.settings.get("pplx-instructions") != null) {
+                        instructions = extensionAPI.settings.get("pplx-instructions");
+                    }
+
+                    var messages = [];
+                    var message;
+                    if (instructions != undefined && instructions != null) {
+                        message = `{"role":"system", "content":"${instructions}"}`;
+                        messages.push(message);
+                    }
+
                     var block = await window.roamAlphaAPI.q(`[:find (pull ?page [:block/string :block/uid]) :where [?page :block/uid "${uid}"] ]`);
                     var query = block[0][0].string.toString();
-                    var body = `{"model":"llama-3.1-sonar-small-128k-online","messages":[{"role":"system","content":"Be precise and concise."},{"role":"user","content":"${query}"}]}`;
+
+                    message = `{"role":"user", "content":"${query}"}`;
+                    messages.push(message);
+
+                    var pplx_url = "https://api.perplexity.ai/chat/completions";
+                    var body = `{"model":"${model}","messages":[${messages}]}`;
+                    console.info(body);
                     const options = {
                         method: 'POST',
                         headers: { Authorization: 'Bearer ' + apiKey, 'Content-Type': 'application/json' },
                         body: body
                     };
-        
+
                     return await fetch(pplx_url, options)
                         .then(response => response.json())
                         .then(response => {
                             console.info(response);
                             var string = response.choices[0].message.content.toString();
-                            var sources = [];
-                            for (var i = 0; i < response.citations.length; i++) {
-                                sources.push({ "text": response.citations[i] });
+                            const regex = /(?<!\*)\*(?!\*)(.*?)(?<!\*)\*(?!\*)/gm;
+                            const subst = `__$1__`;
+                            const regex1 = /### (.+)/gm;
+                            const subst1 = `**$1**`;
+                            string = string.replace(regex, subst);
+                            //string = string.replace(regex1, subst1);
+
+                            if (showSources) {
+                                var sources = [];
+                                for (var i = 0; i < response.citations.length; i++) {
+                                    sources.push({ "text": response.citations[i] });
+                                }
+                                return [
+                                    {
+                                        text: string,
+                                        children: sources
+                                    },
+                                ];
+                            } else {
+                                return [
+                                    {
+                                        text: string
+                                    },
+                                ];
                             }
-        
-                            return [
-                                {
-                                    text: string,
-                                    children: sources
-                                },
-                            ];
                         })
                         .catch(err => console.error(err));
                 }
